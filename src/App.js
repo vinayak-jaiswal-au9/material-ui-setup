a58169b5a72830f8c8e661774c01d166a675519d
vinayak-jaiswal-au8 <vinayakjaiswal121@gmail.com>
2021-06-10 08:06:35 +0530

    'material-ui-setup'

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles/App.css";
+import Box from "@material-ui/core/Box";
+
+function App() {
+  return (
+    <Box className="App" width={300} p={3} boxShadow={1}>
+      <h1>Hii there!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam in at
+        libero consequuntur ex veniam fuga excepturi doloribus nostrum quia
+        repellat provident perspiciatis quasi, consectetur minus eligendi
+        corrupti fugiat ipsum error. Modi, ducimus facere neque molestias
+        necessitatibus ipsa omnis vero blanditiis ut eum. Corrupti, nihil.
+        Maiores, molestiae in! Temporibus, culpa tempore! Voluptatibus
+        accusantium ullam voluptate nam laboriosam unde ad est labore magni,
+        quasi dignissimos quas cupiditate maiores pariatur quidem. Totam,
+        corrupti, perferendis saepe quas voluptatibus vitae repudiandae earum
+        excepturi quaerat, nulla alias explicabo doloremque harum. Vero quis
+        quos tempore earum consectetur itaque placeat porro nesciunt, laudantium
+        incidunt saepe voluptate dolorem.
+      </p>
+    </Box>
+  );
+}
+
+export default App;
